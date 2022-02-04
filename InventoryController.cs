b795b23b81a220b897c628e4c23f9862114cdae6
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-04 15:15:55 +0100

    Video 6 - UIInventoryPage and InventoryController

---
+++ InventoryController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryPage inventoryUI;
+
+    public int inventorySize = 10;
+
+    private void Start()
+    {
+        inventoryUI.InitializeInventoryUI(inventorySize);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+
+        }
+    }
+}
---
+++ UI/UIInventoryPage.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem = 
+                Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel);
+            listOfUIItems.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
