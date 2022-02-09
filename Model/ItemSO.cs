9c3e0326427298c010c8210acbc4ebb5337ef3ef
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-09 14:09:45 +0100

    Video 12 ItemSO

---
+++ Model/ItemSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemSO : ScriptableObject
+{
+    [field: SerializeField]
+    public bool IsStackable { get; set; }
+
+    public int ID => GetInstanceID();
+
+    [field: SerializeField]
+    public int MaxStackSize { get; set; } = 1;
+
+    [field: SerializeField]
+    public string Name { get; set; }
+
+    [field: SerializeField]
+    [field: TextArea]
+    public string Description { get; set; }
+
+    [field: SerializeField]
+    public Sprite ItemImage { get; set; }
+}
