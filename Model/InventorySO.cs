15bc11284abeb2f6a9bd41db595f5dd676aba0c7
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-10 10:51:10 +0100

    Video 13 InventorySO

---
+++ Model/InventorySO.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InventorySO : ScriptableObject
+{
+    [SerializeField]
+    private List<InventoryItem> inventoryItems;
+
+    [field: SerializeField]
+    public int Size { get; private set; } = 10;
+
+    public void Initialize()
+    {
+        inventoryItems = new List<InventoryItem>();
+        for (int i = 0; i < Size; i++)
+        {
+            inventoryItems.Add(InventoryItem.GetEmptyItem());
+        }
+    }
+
+    public void AddItem(ItemSO item, int quantity)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+            {
+                inventoryItems[i] = new InventoryItem
+                {
+                    item = item,
+                    quantity = quantity
+                };
+            }
+        }
+    }
+
+    public Dictionary<int, InventoryItem> GetCurrentInventoryState()
+    {
+        Dictionary<int, InventoryItem> returnValue = 
+            new Dictionary<int, InventoryItem>();
+
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+                continue;
+            returnValue[i] = inventoryItems[i];
+        }
+        return returnValue;
+    }
+}
+
+[Serializable]
+public struct InventoryItem
+{
+    public int quantity;
+    public ItemSO item;
+    public bool IsEmpty => item == null;
+
+    public InventoryItem ChangeQuantity(int newQuantity)
+    {
+        return new InventoryItem
+        {
+            item = this.item,
+            quantity = newQuantity,
+        };
+    }
+
+    public static InventoryItem GetEmptyItem()
+        => new InventoryItem
+        {
+            item = null,
+            quantity = 0,
+        };
+}
