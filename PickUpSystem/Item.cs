5d4dd81799373b1915fce0e42e7a2b2fe8615a5d
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-11 12:40:13 +0100

    Video 18 Picking up items

---
+++ PickUpSystem/Item.cs
@@ -0,0 +1,47 @@
+using Inventory.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [field: SerializeField]
+    public ItemSO InventoryItem { get; private set; }
+
+    [field: SerializeField]
+    public int Quantity { get; set; } = 1;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float duration = 0.3f;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = InventoryItem.ItemImage;
+    }
+
+    public void DestroyItem()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        StartCoroutine(AnimateItemPickup());
+
+    }
+
+    private IEnumerator AnimateItemPickup()
+    {
+        audioSource.Play();
+        Vector3 startScale = transform.localScale;
+        Vector3 endScale = Vector3.zero;
+        float currentTime = 0;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            transform.localScale = 
+                Vector3.Lerp(startScale, endScale, currentTime / duration);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ PickUpSystem/PickUpSystem.cs
@@ -0,0 +1,23 @@
+using Inventory.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSystem : MonoBehaviour
+{
+    [SerializeField]
+    private InventorySO inventoryData;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Item item = collision.GetComponent<Item>();
+        if (item != null)
+        {
+            int reminder = inventoryData.AddItem(item.InventoryItem, item.Quantity);
+            if (reminder == 0)
+                item.DestroyItem();
+            else
+                item.Quantity = reminder;
+        }
+    }
+}
